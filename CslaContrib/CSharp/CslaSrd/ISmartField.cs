f24c14293d5e4775a8099d5ec5760ea5c67f3f73
davidwendelken_cp <SND\davidwendelken_cp>
2007-04-28 04:08:55 +0200

    I corrected the mistake of placing a new version of the C# CslaSrd project files in a sub-directory.

--- CslaContrib/CSharp/CslaSrd/ISmartField.cs
+++ CslaContrib/CSharp/CslaSrd/ISmartField.cs
@@ -4,10 +4,15 @@ using System.Text;
 
 namespace CslaSrd
 {
+    /// <summary>
+    /// This interface is used make it easier to write generic rules against the various Smart datatype classes.
+    /// All the Smart classes (except SmartDate) implement this interface.
+    /// </summary>
     public interface ISmartField
     {
         /// <summary>
-        /// A method, rather than a property, that equates to the IsEmpty property.
+        /// A method, rather than a property, that equates to the IsEmpty property.  
+        /// (Properties cannot be specified in an interface, neither can static methods.)
         /// </summary>
         /// <returns></returns>
         bool HasNullValue();

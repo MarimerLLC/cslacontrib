1bdad3450031eec6ab116d4a673451a310bbab52
jonnybee_cp <SND\jonnybee_cp>
2010-12-31 14:11:28 +0100

    Added missing controls

---
+++ trunk/CslaContrib.Windows/BindingSourceExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace CslaContrib.Windows
+{
+  public static class BindingSourceExtensions
+  {
+    /// <summary>
+    /// Unbind the binding source and the Data object. Use this Method to safely disconnect the data object from a BindingSource before saving data.
+    /// </summary>
+    /// <param name="source">The source.</param>
+    /// <param name="cancel">if set to <c>true</c> then call CancelEdit else call EndEdit.</param>
+    /// <param name="isRoot">if set to <c>true</c> this BindingSource contains the Root object. Set to <c>false</c> for nested BindingSources</param>
+    public static void Unbind(this BindingSource source, bool cancel, bool isRoot)
+    {
+      IEditableObject current = null;
+      // position may be -1 when bindigsource is already unbound
+      // and accessing source.Current will then throw Exception.
+      if ((source.DataSource != null) && (source.Position > -1))
+      {
+        current = source.Current as IEditableObject;
+      }
+
+      // set Raise list changed to True
+      source.RaiseListChangedEvents = false;
+      // tell currency manager to suspend binding
+      source.SuspendBinding();
+
+      if (isRoot) source.DataSource = null;
+      if (current == null) return;
+
+      if (cancel)
+      {
+        current.CancelEdit();
+      }
+      else
+      {
+        current.EndEdit();
+      }
+    }
+
+    /// <summary>
+    /// Rebind the binding source and business object.
+    /// </summary>
+    /// <param name="source">The source.</param>
+    /// <param name="data">The data object.</param>
+    public static void Rebind(this BindingSource source, object data)
+    {
+      source.Rebind(data, false);
+    }
+
+
+    /// <summary>
+    /// Rebind the binding source and business object.
+    /// </summary>
+    /// <param name="source">The source.</param>
+    /// <param name="data">The data object.</param>
+    /// <param name="metadataChanged">if set to <c>true</c> then metadata (type) was changed.</param>
+    public static void Rebind(this BindingSource source, object data, bool metadataChanged)
+    {
+      source.RaiseListChangedEvents = true;
+      source.SuspendBinding();
+
+      if (data != null)
+      {
+        source.DataSource = data;
+      }
+
+      // set Raise list changed to True
+      source.RaiseListChangedEvents = true;
+      // tell currency manager to resume binding 
+      source.ResumeBinding();
+      // Notify UI controls that the dataobject/list was reset - but metadata was not changed 
+      source.ResetBindings(metadataChanged);
+    }
+  }
+}
---
+++ trunk/CslaContrib.Windows/ErrorWarnInfoProvider.designer.cs
@@ -0,0 +1,59 @@
+namespace CslaContrib.Windows
+{
+    partial class ErrorWarnInfoProvider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErrorWarnInfoProvider));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.errorProviderInfo = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderWarn = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderWarn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "error.ico");
+            this.imageList1.Images.SetKeyName(1, "warning.ico");
+            this.imageList1.Images.SetKeyName(2, "INFO.ICO");
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderWarn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ErrorProvider errorProviderInfo;
+        private System.Windows.Forms.ErrorProvider errorProviderWarn;
+    }
+}

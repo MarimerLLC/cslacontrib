1fb65c49afda26393ad309c6208972896ae09922
jonnybee_cp <SND\jonnybee_cp>
2011-01-25 10:16:13 +0100

    Added missing unit test files for CustomFieldData

---
+++ trunk/CslaContrib.CustomFieldData.UnitTests/ChildObject.cs
@@ -0,0 +1,66 @@
+﻿using Csla;
+using System;
+
+namespace CslaContrib.CustomFieldData.UnitTests
+{
+	[Serializable]
+	internal sealed class ChildObject
+		: BusinessCore<ChildObject>
+	{
+		private ChildObject()
+			: base() { }
+
+		internal static ChildObject Create()
+		{
+			return DataPortal.CreateChild<ChildObject>();
+		}
+
+		internal static ChildObject Fetch(string referenceType, bool valueType)
+		{
+			return DataPortal.FetchChild<ChildObject>(
+				new ChildCriteria(referenceType, valueType));
+		}
+
+		private void Child_Fetch(ChildCriteria criteria)
+		{
+			using(this.BypassPropertyChecks)
+			{
+				this.ReferenceType = criteria.ReferenceType;
+				this.ValueType = criteria.ValueType;
+			}
+		}
+
+		private void Child_Insert() { }
+
+		private static PropertyInfo<string> referenceTypeProperty =
+			ChildObject.RegisterProperty<string>(e => e.ReferenceType);
+		public string ReferenceType
+		{
+			get { return this.GetProperty(ChildObject.referenceTypeProperty); }
+			set { this.SetProperty(ChildObject.referenceTypeProperty, value); }
+		}
+
+		private static PropertyInfo<bool> valueTypeProperty =
+			ChildObject.RegisterProperty<bool>(e => e.ValueType);
+		public bool ValueType
+		{
+			get { return this.GetProperty(ChildObject.valueTypeProperty); }
+			set { this.SetProperty(ChildObject.valueTypeProperty, value); }
+		}
+
+		[Serializable]
+		private sealed class ChildCriteria
+			: CriteriaBase<ChildCriteria>
+		{
+			public ChildCriteria(string referenceType, bool valueType)
+				: base()
+			{
+				this.ReferenceType = referenceType;
+				this.ValueType = valueType;
+			}
+
+			public string ReferenceType { get; private set; }
+			public bool ValueType { get; private set; }
+		}
+	}
+}
---
+++ trunk/CslaContrib.CustomFieldData.UnitTests/PropertyInformationFactoryTests.cs
@@ -0,0 +1,52 @@
+﻿using Csla;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spackle.Testing;
+using System;
+
+namespace CslaContrib.CustomFieldData.UnitTests
+{
+	[TestClass]
+	public sealed class PropertyInformationFactoryTests
+		: CoreTests
+	{
+		[TestMethod]
+		public void CreateWithName()
+		{
+			var prop = new PropertyInformationFactory().Create<string>(
+				typeof(PropertyInformationFactoryTests), "name");
+			Assert.IsTrue(typeof(PropertyInformationUsingOriginalValue<string>).IsAssignableFrom(prop.GetType()));
+		}
+
+		[TestMethod]
+		public void CreateWithNameAndFriendlyName()
+		{
+			var prop = new PropertyInformationFactory().Create<string>(
+				typeof(PropertyInformationFactoryTests), "name", "friendlyName");
+			Assert.IsTrue(typeof(PropertyInformationUsingOriginalValue<string>).IsAssignableFrom(prop.GetType()));			
+		}
+
+		[TestMethod]
+		public void CreateWithNameAndFriendlyNameAndRelationship()
+		{
+			var prop = new PropertyInformationFactory().Create<string>(
+				typeof(PropertyInformationFactoryTests), "name", "friendlyName", RelationshipTypes.LazyLoad);
+			Assert.IsTrue(typeof(PropertyInformationUsingOriginalValue<string>).IsAssignableFrom(prop.GetType()));						
+		}
+
+		[TestMethod]
+		public void CreateWithNameAndFriendlyNameAndDefaultValue()
+		{
+			var prop = new PropertyInformationFactory().Create<string>(
+				typeof(PropertyInformationFactoryTests), "name", "friendlyName", (null as string));
+			Assert.IsTrue(typeof(PropertyInformationUsingOriginalValue<string>).IsAssignableFrom(prop.GetType()));									
+		}
+
+		[TestMethod]
+		public void CreateWithNameAndFriendlyNameAndDefaultValueAndRelationship()
+		{
+			var prop = new PropertyInformationFactory().Create<string>(
+				typeof(PropertyInformationFactoryTests), "name", "friendlyName", (null as string), RelationshipTypes.LazyLoad);
+			Assert.IsTrue(typeof(PropertyInformationUsingOriginalValue<string>).IsAssignableFrom(prop.GetType()));												
+		}
+	}
+}

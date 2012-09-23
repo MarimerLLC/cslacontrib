26de417f46c5c821f3f5f0dd4835ddd10ea428d4
tiago_cp <SND\tiago_cp>
2012-09-23 18:31:28 +0200

    Sign all assemblies. Work Item #33152

--- trunk/CslaContrib/Properties/AssemblyInfo.cs
+++ trunk/CslaContrib/Properties/AssemblyInfo.cs
@@ -14,4 +14,5 @@ using System.Runtime.CompilerServices;
 //[assembly: Guid("ba3b547f-fc7c-4ec1-a0f9-ff1a62b2f2c3")]
 
 // Allow unit test project access to internal types and members
-[assembly: InternalsVisibleTo("CslaContrib.UnitTests")]
+// How to find thw PublicKey? sn.exe -Tp CslaContrib.WebGUI.dll
+[assembly: InternalsVisibleTo("CslaContrib.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100cf83b2ddb3b7363c911d441eaa8cb1f0a98f4dd29820e55efe50ccda13b3b446d08308da7136605b4d527b2075f9b67158ae8400ae4cc6a9761c99e778ce536e99b1d89ad3c0c203d452df49c86d34c5f22b5c6d7bc04f2c730ac0c7366f5da155b98ae900577dba1cf25d522888d1a70bf115a7c3a4feac93ed8c83f51eadae")]

55087b9bd86b063b584161e94515fa6a632845cf
tiago_cp <SND\tiago_cp>
2012-09-23 20:22:46 +0200

    Add ActionExtenderSample.WebGUI (port of ActionExtenderSample to WebGUI) Work Item #33155

---
+++ samples/ActionExtenderSample.WebGUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ActionExtenderSample")]
+[assembly: AssemblyDescription("CslaActionExtender sample application for Visual WebGUI")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("ActionExtenderSample")]
+[assembly: AssemblyCulture("")]

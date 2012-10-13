0878f22e6ab931ab0b1aea36d7b69cdb0fa5225d
tiago_cp <SND\tiago_cp>
2012-10-13 21:05:48 +0200

    Remove moved folder.

--- samples/ActionExtenderSample.WebGUI/Properties/GlobalAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("CslaContrib project")]
-[assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.3.13.0")]
-[assembly: AssemblyFileVersion("4.3.13.0")]

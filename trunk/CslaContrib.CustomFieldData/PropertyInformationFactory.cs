494f91c8172f5b376e8f84877e513cdf3d390e24
tiago_cp <SND\tiago_cp>
2012-10-14 02:13:43 +0200

    untabify

--- trunk/CslaContrib.CustomFieldData/PropertyInformationFactory.cs
+++ trunk/CslaContrib.CustomFieldData/PropertyInformationFactory.cs
@@ -7,37 +7,37 @@ namespace CslaContrib.CustomFieldData
   /// <summary>
   /// ProperyInformationFactory class - must be configured in app.config or web.config
   /// </summary>
-	public sealed class PropertyInformationFactory : IPropertyInfoFactory
-	{
-		public PropertyInfo<T> Create<T>(Type containingType, string name, string friendlyName, 
-			T defaultValue, RelationshipTypes relationship)
-		{
-			return new PropertyInformationUsingOriginalValue<T>(containingType, name, friendlyName, 
-				defaultValue, relationship);
-		}
+  public sealed class PropertyInformationFactory : IPropertyInfoFactory
+  {
+    public PropertyInfo<T> Create<T>(Type containingType, string name, string friendlyName, 
+      T defaultValue, RelationshipTypes relationship)
+    {
+      return new PropertyInformationUsingOriginalValue<T>(containingType, name, friendlyName, 
+        defaultValue, relationship);
+    }
 
-		public PropertyInfo<T> Create<T>(Type containingType, string name, string friendlyName, 
-			T defaultValue)
-		{
-			return new PropertyInformationUsingOriginalValue<T>(containingType, name, friendlyName, 
-				defaultValue);
-		}
+    public PropertyInfo<T> Create<T>(Type containingType, string name, string friendlyName, 
+      T defaultValue)
+    {
+      return new PropertyInformationUsingOriginalValue<T>(containingType, name, friendlyName, 
+        defaultValue);
+    }
 
-		public PropertyInfo<T> Create<T>(Type containingType, string name, string friendlyName, 
-			RelationshipTypes relationship)
-		{
-			return new PropertyInformationUsingOriginalValue<T>(containingType, name, friendlyName, 
-				relationship);
-		}
+    public PropertyInfo<T> Create<T>(Type containingType, string name, string friendlyName, 
+      RelationshipTypes relationship)
+    {
+      return new PropertyInformationUsingOriginalValue<T>(containingType, name, friendlyName, 
+        relationship);
+    }
 
-		public PropertyInfo<T> Create<T>(Type containingType, string name, string friendlyName)
-		{
-			return new PropertyInformationUsingOriginalValue<T>(containingType, name, friendlyName);
-		}
+    public PropertyInfo<T> Create<T>(Type containingType, string name, string friendlyName)
+    {
+      return new PropertyInformationUsingOriginalValue<T>(containingType, name, friendlyName);
+    }
 
-		public PropertyInfo<T> Create<T>(Type containingType, string name)
-		{
-			return new PropertyInformationUsingOriginalValue<T>(containingType, name);
-		}
-	}
+    public PropertyInfo<T> Create<T>(Type containingType, string name)
+    {
+      return new PropertyInformationUsingOriginalValue<T>(containingType, name);
+    }
+  }
 }
--- trunk/CslaContrib.CustomFieldData/PropertyInformationUsingOriginalValue.cs
+++ trunk/CslaContrib.CustomFieldData/PropertyInformationUsingOriginalValue.cs
@@ -8,49 +8,49 @@ namespace CslaContrib.CustomFieldData
   /// ProperyInformationFactory class - must be configured in app.config or web.config
   /// </summary>
   /// <typeparam name="T"></typeparam>
-	public sealed class PropertyInformationUsingOriginalValue<T>
-		: PropertyInfo<T>
-	{
-		public PropertyInformationUsingOriginalValue(Type containingType, string name)
-			: base(name)
-		{
-			this.ContainingType = containingType;
-		}
+  public sealed class PropertyInformationUsingOriginalValue<T>
+    : PropertyInfo<T>
+  {
+    public PropertyInformationUsingOriginalValue(Type containingType, string name)
+      : base(name)
+    {
+      this.ContainingType = containingType;
+    }
 
-		public PropertyInformationUsingOriginalValue(Type containingType, string name, string friendlyName)
-			: base(name, friendlyName)
-		{
-			this.ContainingType = containingType;
-		}
+    public PropertyInformationUsingOriginalValue(Type containingType, string name, string friendlyName)
+      : base(name, friendlyName)
+    {
+      this.ContainingType = containingType;
+    }
 
-		public PropertyInformationUsingOriginalValue(Type containingType, string name, string friendlyName,
-			RelationshipTypes relationship)
-			: base(name, friendlyName, relationship)
-		{
-			this.ContainingType = containingType;
-		}
+    public PropertyInformationUsingOriginalValue(Type containingType, string name, string friendlyName,
+      RelationshipTypes relationship)
+      : base(name, friendlyName, relationship)
+    {
+      this.ContainingType = containingType;
+    }
 
-		public PropertyInformationUsingOriginalValue(Type containingType, string name, string friendlyName,
-			T defaultValue)
-			: base(name, friendlyName, defaultValue)
-		{
-			this.ContainingType = containingType;
-		}
+    public PropertyInformationUsingOriginalValue(Type containingType, string name, string friendlyName,
+      T defaultValue)
+      : base(name, friendlyName, defaultValue)
+    {
+      this.ContainingType = containingType;
+    }
 
-		public PropertyInformationUsingOriginalValue(Type containingType, string name, string friendlyName,
-			T defaultValue, RelationshipTypes relationship)
-			: base(name, friendlyName, defaultValue, relationship)
-		{
-			this.ContainingType = containingType;
-		}
+    public PropertyInformationUsingOriginalValue(Type containingType, string name, string friendlyName,
+      T defaultValue, RelationshipTypes relationship)
+      : base(name, friendlyName, defaultValue, relationship)
+    {
+      this.ContainingType = containingType;
+    }
 
-		protected override IFieldData NewFieldData(string name)
-		{
-			return typeof(T).IsAssignableFrom(typeof(string)) ?
-				new FieldDataUsingOriginalValueViaHashCode<T>(name) as IFieldData :
-				new FieldDataUsingOriginalValueViaDuplicate<T>(name) as IFieldData;
-		}
+    protected override IFieldData NewFieldData(string name)
+    {
+      return typeof(T).IsAssignableFrom(typeof(string)) ?
+        new FieldDataUsingOriginalValueViaHashCode<T>(name) as IFieldData :
+        new FieldDataUsingOriginalValueViaDuplicate<T>(name) as IFieldData;
+    }
 
-		private Type ContainingType { get; set; }
-	}
+    private Type ContainingType { get; set; }
+  }
 }

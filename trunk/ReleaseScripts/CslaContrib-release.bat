cf496731544dffb675c069450ac5ebca7235f652
tiago_cp <SND\tiago_cp>
2012-10-15 03:11:32 +0200

    Minor changes to release scripts Work Item #33234

--- trunk/ReleaseScripts/CslaContrib-release.bat
+++ trunk/ReleaseScripts/CslaContrib-release.bat
@@ -7,7 +7,8 @@ xcopy /E ..\..\bin\Release
 svn export https://cslacontrib.svn.codeplex.com/svn/trunk/License.txt
 svn export https://cslacontrib.svn.codeplex.com/svn/trunk/Readme.txt
 
-"C:\Program Files\7-Zip\7z" a -r -tzip ..\CslaContribSource *
+del /q ..\CslaContrib.zip
+"C:\Program Files\7-Zip\7z" a -r -tzip ..\CslaContrib *
 
 cd ..
 rd /q/s Release
--- trunk/ReleaseScripts/CslaContribSamples-release.bat
+++ trunk/ReleaseScripts/CslaContribSamples-release.bat
@@ -2,11 +2,12 @@ rd /q /s Release
 md Release
 cd Release
 
-svn export https://cslacontrib.svn.codeplex.com/svn/samples CslaContribSamples
+svn export https://cslacontrib.svn.codeplex.com/svn/trunk/samples CslaContribSamples
 
 svn export https://cslacontrib.svn.codeplex.com/svn/trunk/License.txt
 svn export https://cslacontrib.svn.codeplex.com/svn/trunk/Readme.txt
 
+del /q ..\CslaContribSamples.zip
 "C:\Program Files\7-Zip\7z" a -r -tzip ..\CslaContribSamples *
 
 cd ..

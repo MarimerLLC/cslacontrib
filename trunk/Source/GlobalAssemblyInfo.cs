cd2ee30be00d7c5c998bc99616c1208c6a90d56a
tiago_cp <SND\tiago_cp>
2013-03-26 00:19:04 +0100

    Update version to 4.3.14

--- trunk/Source/GlobalAssemblyInfo.cs
+++ trunk/Source/GlobalAssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("CslaContrib project")]
-[assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2012")]
+[assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.3.13.0")]
-[assembly: AssemblyFileVersion("4.3.13.0")]
+[assembly: AssemblyVersion("4.3.14.0")]
+[assembly: AssemblyFileVersion("4.3.14.0")]

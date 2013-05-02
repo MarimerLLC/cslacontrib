f4d26aa9fd254bbe71db856b11b78bf61c1eb8ce
tiago_cp <SND\tiago_cp>
2013-05-02 21:50:52 +0200

    Change sample version to 4.5.20 Work Item #33920

--- trunk/samples/ActionExtenderSample.WebGUI/ActionExtenderSample/Properties/GlobalAssemblyInfo.cs
+++ trunk/samples/ActionExtenderSample.WebGUI/ActionExtenderSample/Properties/GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.3.14.0")]
-[assembly: AssemblyFileVersion("4.3.14.0")]
+[assembly: AssemblyVersion("4.5.20.0")]
+[assembly: AssemblyFileVersion("4.5.20.0")]

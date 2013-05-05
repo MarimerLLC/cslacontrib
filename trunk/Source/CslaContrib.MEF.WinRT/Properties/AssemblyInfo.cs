eb7b7eeb312cbe56ad85b867890276c2230487da
tiago_cp <SND\tiago_cp>
2013-05-05 16:06:34 +0200

    CslaContrib.MEF for WinRT improvements: - sign sssembly - default build is AnyCPU - use GlobalAssemplyInfo.cs

--- trunk/Source/CslaContrib.MEF.WinRT/Properties/AssemblyInfo.cs
+++ trunk/Source/CslaContrib.MEF.WinRT/Properties/AssemblyInfo.cs
@@ -1,29 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CslaContrib.MEF")]
-[assembly: AssemblyDescription("CslaContrib.MEF for WindowsRT")]
+[assembly: AssemblyTitle("CslaContrib.MEF for WindowsRT")]
+[assembly: AssemblyDescription("Extends CSLA .NET framework with MEF support for Repository and ObjectFactory data access.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CslaContrib.MEF")]
-[assembly: AssemblyCopyright("Copyright © 2012")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.5.13.0")]
-[assembly: AssemblyFileVersion("4.5.13.0")]
-[assembly: ComVisible(false)]

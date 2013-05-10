d93cc7d19caedb630dc45337a9f6f46e658d4b83
tiago_cp <SND\tiago_cp>
2013-05-11 00:59:20 +0200

    Update version to 4.5.30 (match Csla release) Work Item #34032

--- trunk/Source/GlobalAssemblyInfo.cs
+++ trunk/Source/GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.5.20")]
-[assembly: AssemblyFileVersion("4.5.20")]
+[assembly: AssemblyVersion("4.5.30")]
+[assembly: AssemblyFileVersion("4.5.30")]

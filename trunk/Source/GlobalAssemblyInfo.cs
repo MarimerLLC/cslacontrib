e184d5e77a94b6669940a4c3017c270d13cd8278
tiago_cp <SND\tiago_cp>
2013-05-25 13:41:30 +0200

    Release 4.5.31 beta on NuGet Work Item #34093

--- trunk/Source/GlobalAssemblyInfo.cs
+++ trunk/Source/GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.5.30")]
-[assembly: AssemblyFileVersion("4.5.30")]
+[assembly: AssemblyVersion("4.5.31")]
+[assembly: AssemblyFileVersion("4.5.31")]
--- trunk/samples/ActionExtenderSample.WebGUI/GlobalAssemblyInfo.cs
+++ trunk/samples/ActionExtenderSample.WebGUI/GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.5.30.0")]
-[assembly: AssemblyFileVersion("4.5.30.0")]
+[assembly: AssemblyVersion("4.5.31")]
+[assembly: AssemblyFileVersion("4.5.31")]

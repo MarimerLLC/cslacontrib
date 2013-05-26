d3166665237d1fb780d101812181b04c27277bec
tiago_cp <SND\tiago_cp>
2013-05-26 20:46:50 +0200

    Add SmartDateExtendedParserSample for Silverlight 5 Work Item #34083

---
+++ trunk/samples/SmartDateExtendedParserSample/SmartDateExtendedParserSample.Sl/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SmartDateExtendedParserSample in Silverlight")]
+[assembly: AssemblyDescription("Sample application for SmartDateExtendedParserSample")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("SmartDateExtendedParserSample")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("03b5a3f2-c21c-4c36-ad9c-b29329676ce7")]

ca26b4262943a9eb4fd67d9c812831bcfb0f0520
tiago_cp <SND\tiago_cp>
2013-05-26 20:50:02 +0200

    Restructure files on all samples: sign all assemblies, one single file nuget.config and for GlobalAssemblyInfo.cs Work Item #34083

--- trunk/samples/MEFSamples/GlobalAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("CslaContrib project")]
-[assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.5.31")]
-[assembly: AssemblyFileVersion("4.5.31")]

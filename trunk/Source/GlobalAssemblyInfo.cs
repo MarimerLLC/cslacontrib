506f0e6d185a61cef5bfb43417fe300f41c352e1
tiago_cp <SND\tiago_cp>
2014-09-17 22:31:45 +0200

    Version update
    Work Item #35376

--- trunk/Source/GlobalAssemblyInfo.cs
+++ trunk/Source/GlobalAssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("CslaContrib project")]
-[assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2013")]
+[assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.5.50")]
-[assembly: AssemblyFileVersion("4.5.50")]
+[assembly: AssemblyVersion("4.5.600")]
+[assembly: AssemblyFileVersion("4.5.600")]

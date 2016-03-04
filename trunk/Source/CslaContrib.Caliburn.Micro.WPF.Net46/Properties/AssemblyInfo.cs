08b14f1ef1df0b43e25eb82612ae2c1a9a863aeb
tfreitasleal <tfreitasleal@gmail.com>
2016-03-04 00:04:04 +0000

    Add empty .Net46 projects

---
+++ trunk/Source/CslaContrib.Caliburn.Micro.WPF.Net46/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CslaContrib.Caliburn.Micro 1.5.2 for WPF in .NET 4.6")]
+[assembly: AssemblyDescription("CSLA .NET ViewModel adapter for Caliburn.Micro v.1.5.2.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("CslaContrib.Caliburn.Micro.WPF.NET46")]
+[assembly: AssemblyCulture("")]
---
+++ trunk/Source/CslaContrib.MEF.Net46/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CslaContrib.MEF in .NET 4.6")]
+[assembly: AssemblyDescription("Extends CSLA .NET framework with MEF support for Repository and ObjectFactory data access.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("CslaContrib.MEF.NET46")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("01099120-bb31-46fa-a176-a577c0ec9b1f")]

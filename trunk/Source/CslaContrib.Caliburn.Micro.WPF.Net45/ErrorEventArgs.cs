e484a4112c370482fe9ce1b93cb7f213fdd66b77
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-09-03 22:18:28 +0100

    Discontinue Silverlight support (#40). Update CSLA to 4.6.603

---
+++ trunk/Source/CslaContrib.Caliburn.Micro.WPF.Net45/ErrorEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace CslaContrib.Caliburn.Micro
+{
+    using System;
+
+    /// <summary>
+    /// Contains information about the error that
+    /// has occurred.
+    /// </summary>
+    public class ErrorEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the Exception object for the error
+        /// that occurred.
+        /// </summary>
+        public Exception Error { get; internal set; }
+    }
+}

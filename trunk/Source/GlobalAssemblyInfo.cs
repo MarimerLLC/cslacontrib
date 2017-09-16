ae34a62fd853d14d85ec42d5d9f37c92d4d9160c
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-09-16 18:36:50 +0100

    Version 4.6.602 (#61)

--- trunk/Source/GlobalAssemblyInfo.cs
+++ trunk/Source/GlobalAssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("CslaContrib project")]
-[assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2016")]
+[assembly: AssemblyCopyright("Copyright © CslaContrib 2009-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.6.601")]
-[assembly: AssemblyFileVersion("4.6.601")]
+[assembly: AssemblyVersion("4.6.602")]
+[assembly: AssemblyFileVersion("4.6.602")]

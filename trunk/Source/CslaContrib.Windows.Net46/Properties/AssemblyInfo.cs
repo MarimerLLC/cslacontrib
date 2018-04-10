047b78a1758cd2cde7f966a708082538531b9323
tfreitasleal <tfreitasleal@gmail.com>
2018-04-10 17:01:52 +0100

    Retarget 4.6 to 4.6.1

--- trunk/Source/CslaContrib.Windows.Net46/Properties/AssemblyInfo.cs
+++ trunk/Source/CslaContrib.Windows.Net46/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@
 [assembly: AssemblyTitle("CslaContrib.Windows in .NET 4.6")]
 [assembly: AssemblyDescription("Additional controls to support the creation of CSLA. NET applications for Windows Forms.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("CslaContrib.Windows.NET46")]
+[assembly: AssemblyProduct("CslaContrib.Windows.NET461")]
 [assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM

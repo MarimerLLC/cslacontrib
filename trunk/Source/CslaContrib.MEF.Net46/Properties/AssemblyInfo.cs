c4d251e37d5e52a134f87ca8fa54334d117c9edc
tfreitasleal <tfreitasleal@gmail.com>
2018-04-10 17:01:17 +0100

    Retarget 4.6 to 4.6.1

--- trunk/Source/CslaContrib.MEF.Net46/Properties/AssemblyInfo.cs
+++ trunk/Source/CslaContrib.MEF.Net46/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@
 [assembly: AssemblyTitle("CslaContrib.MEF in .NET 4.6")]
 [assembly: AssemblyDescription("Extends CSLA .NET framework with MEF support for Repository and ObjectFactory data access.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("CslaContrib.MEF.NET46")]
+[assembly: AssemblyProduct("CslaContrib.MEF.NET461")]
 [assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM

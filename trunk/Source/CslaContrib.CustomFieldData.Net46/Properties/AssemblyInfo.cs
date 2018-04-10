f9d932d07b060b8783bfe4a34e6143ed01abf4f1
tfreitasleal <tfreitasleal@gmail.com>
2018-04-10 17:01:06 +0100

    Retarget 4.6 to 4.6.1

--- trunk/Source/CslaContrib.CustomFieldData.Net46/Properties/AssemblyInfo.cs
+++ trunk/Source/CslaContrib.CustomFieldData.Net46/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("CslaContrib.CustomFieldData in .NET 4.6")]
 [assembly: AssemblyDescription("Extends CSLA .NET framework with Custom Field Data that checks old/new value to determine if the object is dirty/changed.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("CslaContrib.CustomFieldData.NET46")]
+[assembly: AssemblyProduct("CslaContrib.CustomFieldData.NET461")]
 [assembly: AssemblyCulture("")]
 
 // Mark the assembly as CLS compliant

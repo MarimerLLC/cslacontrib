a4e2976d238432b2f8f7684ba44af0f58021c688
tfreitasleal <tfreitasleal@gmail.com>
2018-04-10 17:00:53 +0100

    Retarget 4.6 to 4.6.1

--- trunk/Source/CslaContrib.Caliburn.Micro.WPF.Net46/Properties/AssemblyInfo.cs
+++ trunk/Source/CslaContrib.Caliburn.Micro.WPF.Net46/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@
 [assembly: AssemblyTitle("CslaContrib.Caliburn.Micro 1.5.2 for WPF in .NET 4.6")]
 [assembly: AssemblyDescription("CSLA .NET ViewModel adapter for Caliburn.Micro v.1.5.2.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("CslaContrib.Caliburn.Micro.WPF.NET46")]
+[assembly: AssemblyProduct("CslaContrib.Caliburn.Micro.WPF.NET461")]
 [assembly: AssemblyCulture("")]

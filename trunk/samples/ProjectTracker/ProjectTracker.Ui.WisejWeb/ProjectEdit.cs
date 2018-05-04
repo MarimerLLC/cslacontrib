ed548e068a0ce0245c162614b95805d93b8e3809
tfreitasleal <tfreitasleal@gmail.com>
2018-05-04 09:51:32 +0100

    Revert to RoleList sync cache refresh (#137)

--- trunk/samples/ProjectTracker/ProjectTracker.Ui.WisejWeb/ProjectEdit.cs
+++ trunk/samples/ProjectTracker/ProjectTracker.Ui.WisejWeb/ProjectEdit.cs
@@ -184,7 +184,7 @@ namespace PTWisej
       this.Close();
     }
 
-    private async void RefreshButton_Click(object sender, EventArgs e)
+    private void RefreshButton_Click(object sender, EventArgs e)
     {
       using (StatusBusy busy = new StatusBusy("Refreshing..."))
       {
@@ -192,7 +192,7 @@ namespace PTWisej
         {
           Project = ProjectTracker.Library.ProjectEdit.GetProject(Project.Id);
           RoleList.InvalidateCache();
-          await RoleList.CacheListAsync();
+          RoleList.CacheList();
           Setup();
         }
       }

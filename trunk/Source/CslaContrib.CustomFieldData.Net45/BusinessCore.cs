024b854c740ba0a361c07fde4c4f797b57d9ed19
Filip <filip.jelic93@gmail.com>
2018-09-25 11:08:27 +0200

    Removed unused using.

--- trunk/Source/CslaContrib.CustomFieldData.Net45/BusinessCore.cs
+++ trunk/Source/CslaContrib.CustomFieldData.Net45/BusinessCore.cs
@@ -1,54 +1,53 @@
 ﻿using Csla;
 using System;
-using Csla.Serialization;
 
 namespace CslaContrib.CustomFieldData
 {
-	[Serializable]
-	public abstract class BusinessCore<T> : BusinessBase<T>
-		where T : BusinessCore<T>
-	{
-		protected BusinessCore()
-			: base() { }
+  [Serializable]
+  public abstract class BusinessCore<T> : BusinessBase<T>
+        where T : BusinessCore<T>
+  {
+    protected BusinessCore()
+        : base() { }
 
-		protected override void Child_OnDataPortalInvokeComplete(DataPortalEventArgs e)
-		{
-			base.Child_OnDataPortalInvokeComplete(e);
-			this.HandleOnDataPortalComplete(e);
-		}
+    protected override void Child_OnDataPortalInvokeComplete(DataPortalEventArgs e)
+    {
+      base.Child_OnDataPortalInvokeComplete(e);
+      this.HandleOnDataPortalComplete(e);
+    }
 
-		protected override void DataPortal_OnDataPortalInvokeComplete(DataPortalEventArgs e)
-		{
-			base.DataPortal_OnDataPortalInvokeComplete(e);
-			this.HandleOnDataPortalComplete(e);
-		}
+    protected override void DataPortal_OnDataPortalInvokeComplete(DataPortalEventArgs e)
+    {
+      base.DataPortal_OnDataPortalInvokeComplete(e);
+      this.HandleOnDataPortalComplete(e);
+    }
 
-		private void HandleOnDataPortalComplete(DataPortalEventArgs e)
-		{
-			if(e.Operation == DataPortalOperations.Create)
-			{
-				this.MarkClean();
-			}
-		}
+    private void HandleOnDataPortalComplete(DataPortalEventArgs e)
+    {
+      if (e.Operation == DataPortalOperations.Create)
+      {
+        this.MarkClean();
+      }
+    }
 
-		public override bool IsSelfDirty
-		{
-			get
-			{
-				var isSelfDirty = false;
+    public override bool IsSelfDirty
+    {
+      get
+      {
+        var isSelfDirty = false;
 
-				foreach(var registeredProperty in this.FieldManager.GetRegisteredProperties())
-				{
-					if(registeredProperty.RelationshipType.HasFlag(RelationshipTypes.Child) &&
-						this.FieldManager.IsFieldDirty(registeredProperty))
-					{
-						isSelfDirty = true;
-						break;
-					}
-				}
+        foreach (var registeredProperty in this.FieldManager.GetRegisteredProperties())
+        {
+          if (registeredProperty.RelationshipType.HasFlag(RelationshipTypes.Child) &&
+              this.FieldManager.IsFieldDirty(registeredProperty))
+          {
+            isSelfDirty = true;
+            break;
+          }
+        }
 
-				return isSelfDirty;
-			}
-		}
-	}
+        return isSelfDirty;
+      }
+    }
+  }
 }
